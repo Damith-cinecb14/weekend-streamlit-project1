1c7ee74bd340165c6aef39e6a5477a11a659bfc3
unknown <damithccna2015lasantha@gmail.com>
2026-03-08 11:53:04 +0530

    my python 3.10 version project for created a question and answering streamlit app power by NLP model

---
+++ app.py
@@ -0,0 +1,33 @@
+import  streamlit as st
+from transformers import pipeline
+
+st.set_page_config(page_title="AI Question and Answering app")
+
+@st.cache_resource
+def load_model():
+    return pipeline("question-answering", "timpal0l/mdeberta-v3-base-squad2")
+
+qa_model = load_model()
+
+st.title("AI Powered Question and Answering webApp")
+
+col1, col2 = st.columns([2,1])
+
+with col1:
+    context = st.text_area("Text area for context",
+                           height=150, placeholder="Enter Context here....")
+    question = st.text_area("Text area for question", height=150,placeholder="Ask your question here")
+    submit_btn = st.button("Ask Question", type="primary")
+
+with col2:
+    st.markdown("Powered by C-Clarke Institute students")
+
+if context and question and submit_btn:
+    with st.spinner("Answering your questions........"):
+        result = qa_model(question=question,context=context)
+        st.success(result['answer'])
+        st.metric(result['score'])
+
+else:
+    st.markdown("invalid Input....")
+
