99ab56298a642e35f84ee2b276175d3614de1e35
unknown <damithccna2015lasantha@gmail.com>
2026-03-08 15:04:57 +0530

    all changes for original code for workig for score

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ with col2:
 if context and question and submit_btn:
     with st.spinner("Answering your questions........"):
         result = qa_model(question=question,context=context)
-        st.success(result['answer'])
         st.metric(result['score'])
+        st.metric("Confidence Score", round(result["score"], 3))
 
 else:
     st.markdown("invalid Input....")
